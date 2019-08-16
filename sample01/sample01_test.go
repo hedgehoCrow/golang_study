92b406ec9cd842980328ff698a49345ecff426b1
hedgehocrow <crowizard@gmail.com>
2019-08-16 20:42:26 +0900

    feat: add go sample test

---
+++ sample01/sample01_test.go
@@ -0,0 +1,14 @@
+package sample01
+
+import (
+    "testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+    actual := HelloWorld("hoge")
+    exptedted := "hello world, hoge"
+    if actual != exptedted {
+        t.Errorf("actual %v\nwant %v", actual, exptedted)
+    }
+}
+
